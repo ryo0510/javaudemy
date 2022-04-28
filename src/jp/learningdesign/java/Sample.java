12c3f77ac821f1175eabfc750fcd91b73a30e8a9
ryo0510 <94730431+ryo0510@users.noreply.github.com>
2022-04-28 16:11:37 +0900

    (start)4-41

--- src/jp/learningdesign/java/Sample.java
+++ src/jp/learningdesign/java/Sample.java
@@ -11,7 +11,7 @@ public class Sample {
  */
 	public static void main(String[] args) {
 		// 次の行でコンソールに文字列を出力します
-		System.out.println("Hello Eclipse");
+		System.out.println("Hello Eclipse\nIt`s Tuesday today.");
 
 	}
 
---
+++ src/jp/learningdesign/java/calc.java
@@ -0,0 +1,10 @@
+package jp.learningdesign.java;
+
+public class calc {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
