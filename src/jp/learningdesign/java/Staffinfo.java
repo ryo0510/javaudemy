6e9824f7c8995668cae6aab6d9e2f817e942d14c
ryo0510 <94730431+ryo0510@users.noreply.github.com>
2022-04-28 15:06:11 +0900

    (finish)2-30

--- src/jp/learningdesign/java/Staffinfo.java
+++ src/jp/learningdesign/java/Staffinfo.java
@@ -10,6 +10,9 @@ public class Staffinfo {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Staff yamada = new Staff();
+		yamada.name = "Taro Yamada";
+		
+		System.out.println(yamada.name);
 
 	}
 
