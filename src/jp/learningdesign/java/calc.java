a865b84c35d9a4a4a59974ee47c1f6a1efb864ce
ryo0510 <94730431+ryo0510@users.noreply.github.com>
2022-04-28 16:20:09 +0900

    (finish)4-41

--- src/jp/learningdesign/java/calc.java
+++ src/jp/learningdesign/java/calc.java
@@ -3,7 +3,14 @@ package jp.learningdesign.java;
 public class calc {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// 算術演算子 + - * / %
+		int a = 15;
+		int b = 4;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
 
 	}
 
