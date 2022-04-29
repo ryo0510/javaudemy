b5291f1e7010f56be3228829acfc943632c9bc03
ryo0510 <94730431+ryo0510@users.noreply.github.com>
2022-04-29 16:54:12 +0900

    (finish)4-47

--- src/jp/learningdesign/java/CountDown.java
+++ src/jp/learningdesign/java/CountDown.java
@@ -1,9 +1,9 @@
 package jp.learningdesign.java;
 
-public class CountUp {
+public class CountDown {
 
 	public static void main(String[] args) {
-		for (int i = 0;i < 10; i++) {
+		for (int i = 10; i > 0; i--) {
 			System.out.println(i);
 		}
 
