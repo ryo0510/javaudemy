cce3b145151c10d6a9fa03a925ee0e9ca7dd7c9c
ryo0510 <94730431+ryo0510@users.noreply.github.com>
2022-04-29 17:28:02 +0900

    (finish)4-49-キーボード入力値からBMI、判定結果を表示してみよう

--- src/jp/learningdesign/java/BmiCalcIf2.java
+++ src/jp/learningdesign/java/BmiCalcIf2.java
@@ -1,10 +1,37 @@
 package jp.learningdesign.java;
 
+import java.util.Scanner;
+
 public class BmiCalcIf2 {
+/* BMI(体格指数)を計算するプログラム
+ * BMI = 体重 / (身長 * 身長)
+ * 体重 kg、身長 cm
+ */
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		double weight,height,bmi;
+		
+		//weight = 71;
+		//height = 180;
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("体重は？(kg)");
+		weight = stdIn.nextDouble();
+		System.out.println("身長は？(cm)");
+		height = stdIn.nextDouble();
+		
+		height /= 100;
+		
+		bmi = weight / (height * height);
+		
+		System.out.println("あなたのBMIは、" + bmi + "です。");
+		
+		if (bmi < 18.5) {
+			System.out.println("やせ型です");
+		} else if (bmi < 25) {
+			System.out.println("標準です");			
+		} else {
+			System.out.println("肥満です");
+		}
 	}
 
 }
