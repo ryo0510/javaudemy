193f8b28a1615f349e1bb5470b01659c59cf51eb
ryo0510 <94730431+ryo0510@users.noreply.github.com>
2022-04-29 15:46:59 +0900

    (finish)4-43

--- src/jp/learningdesign/java/BmiCalc.java
+++ src/jp/learningdesign/java/BmiCalc.java
@@ -1,10 +1,19 @@
 package jp.learningdesign.java;
 
 public class BmiCalc {
+/* BMI(体格指数)を計算するプログラム
+ * BMI = 体重 / (身長 * 身長)
+ * 体重 kg、身長 m
+ */
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		double weight,height,bmi;
+		
+		weight = 71;
+		height = 1.8;
+		bmi = weight / (height * height);
+		
+		System.out.println(bmi);
 	}
 
 }
