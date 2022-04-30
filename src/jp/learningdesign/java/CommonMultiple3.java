f9026a5f0ac3799af471d27e5884ee684dbaaba4
ryo0510 <3203ryo@gmail.com>
2022-04-30 17:01:32 +0900

    (finish)4-53-反転(NOT演算子)

---
+++ src/jp/learningdesign/java/CommonMultiple3.java
@@ -0,0 +1,18 @@
+package jp.learningdesign.java;
+
+public class CommonMultiple3 {
+
+	public static void main(String[] args) {
+		// 2と3の公倍数以外の数を表示する
+		int i = 1;
+		while (i < 20) {
+			if (!((i % 2 == 0) && (i % 3 == 0))) { 
+				// NOT演算子の適用例
+				System.out.println(i);
+			}
+			i++; // インクリメント演算子
+		}
+
+	}
+
+}
