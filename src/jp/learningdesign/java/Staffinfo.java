4e63d4055f1052d5dc3adc35d33b136a8eb9c94f
ryo0510 <3203ryo@gmail.com>
2022-05-01 14:55:12 +0900

    (finish)5-57_コンストラクタを定義してみよう

--- src/jp/learningdesign/java/Staffinfo.java
+++ src/jp/learningdesign/java/Staffinfo.java
@@ -8,14 +8,21 @@ class Staff {
 	public void sayhello() {
 		System.out.println("Hello " + this.name);
 	}
+
+	public Staff(String name, int staffid, String email) {
+		super();
+		this.name = name;
+		this.staffid = staffid;
+		this.email = email;
+	}
 	
 }
+
 public class Staffinfo {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		Staff yamada = new Staff();
-		yamada.name = "Taro Yamada";
+		Staff yamada = new Staff("Taro Yamada",12345,"yamada@gmail.com");
+		// yamada.name = "Taro Yamada";
 		
 		yamada.sayhello();
 		
