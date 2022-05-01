5abe6fe82e8b71a8bdeddb997d37412c9c3e9eb7
ryo0510 <3203ryo@gmail.com>
2022-05-01 15:02:48 +0900

    (finish)5-58_ゲッターメソッドを定義しよう

--- src/jp/learningdesign/java/Staffinfo.java
+++ src/jp/learningdesign/java/Staffinfo.java
@@ -15,6 +15,30 @@ class Staff {
 		this.staffid = staffid;
 		this.email = email;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStaffid() {
+		return staffid;
+	}
+
+	public void setStaffid(int staffid) {
+		this.staffid = staffid;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 }
 
@@ -24,9 +48,12 @@ public class Staffinfo {
 		Staff yamada = new Staff("Taro Yamada",12345,"yamada@gmail.com");
 		// yamada.name = "Taro Yamada";
 		
-		yamada.sayhello();
+		// yamada.sayhello();
 		
-		// System.out.println(yamada.name);
+		System.out.println("【社員情報】");
+		System.out.println("氏名: " + yamada.getName());
+		System.out.println("社員番号: " + yamada.getStaffid());
+		System.out.println("Email: " + yamada.getEmail());
 
 	}
 
